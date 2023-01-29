0a569e299f329a906beb30034425c91eb1ce355b
Juanfran Montero <juanfrancisco.montero@estudiantes.uva.es>
2023-01-29 13:09:32 +0100

    Vistas de comunidad y seguidos implementada

--- lazarus/inc/helpers/upload_helper_msg.php
+++ lazarus/inc/helpers/upload_helper_msg.php
@@ -12,44 +12,35 @@
   $controlImagen = getimagesize($messagePic["tmp_name"]);
 
   if ($controlImagen !== false) {
-    #echo "El fichero es una imagen - " . $controlImagen["mime"] . ".";
     $uploadOK = 1;
   } else {
-    #echo "El fichero no es una imagen.";
     $uploadOK = 0;
   }
   
 
   // Comprobar si el fichero ya existe (en el servidor)
   if (file_exists($nom_fich_subido)) {
-    #echo "Lo siento, el fichero ya existe.";
     $uploadOK = 0;
   }
 
   // Comprobar el tamaño de la imagen
   if ($messagePic["size"] > 8388608) {
-    #echo "Lo siento, la imagen es muy grande.";
     $uploadOK = 0;
   }
 
   // Permitir solo ciertos formatos de fichero
   if ($tipo_fich_img != "jpg" && $tipo_fich_img != "png" && $tipo_fich_img != "jpeg" && $tipo_fich_img != "gif") {
-    #echo "Lo siento, solo se permiten ficheros JPG, JPEG, PNG o GIF.";
     $uploadOK = 0;
   }
 
   // Comprobar si ha habido algún error
   if ($uploadOK == 0) {
-    #echo "Lo siento, tu fichero no se ha subido. Se ha puesto la imagen por def.";
     $_SESSION['img_msg_error'] = "Lo siento, la imagen no se proceso correctamente.";
     // Si todo está bien, tratar de subir el fichero
   } else {
     if (copy($messagePic["tmp_name"], $nom_fich_subido)) {
-      #echo "Se ha subido el fichero " . basename($messagePic["name"]);
-      #echo '<p>' . "Se ha subido el fichero " . $nom_fich_subido . '</p>';
       $this->col_usrPost_media = $nom_fich_subido;
     } else {
-      #echo "Lo siento, ha habido un error al subir tu fichero.";
       $_SESSION['img_msg_error'] = "Lo siento, la imagen no se almacenó bien.";
     }
   }
